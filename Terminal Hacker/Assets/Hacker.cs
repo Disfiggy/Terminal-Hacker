950df3108e4e702d1ef72241f44f6a9d95b30d78
Disfigure <eero.lamminmaki@gmail.com>
2017-11-23 18:17:12 +0200

    First commit for Terminal Hacker

---
+++ Terminal Hacker/Assets/Hacker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MainStoryIntroduction();
+    }
+	
+    void MainStoryIntroduction ()
+    {
+        Terminal.WriteLine("Hello fellow traveler.");
+        Terminal.WriteLine(" ");
+        Terminal.WriteLine("You have entered the world of Xanion 281-Z through the gate system.");
+        Terminal.WriteLine("The world of Xanion is a digital world that is due to its code basis visualized as highly deserted world.");
+        Terminal.WriteLine(" ");
+        Terminal.WriteLine("There are sand dunes as far as they eye can see, but in the distance you are able to distinguish a rock like construction");
+        Terminal.WriteLine("The sun is going to be setting in a few hours and after that the environment will most likely become uninhabitable for any human");
+        Terminal.WriteLine(" ");
+        Terminal.WriteLine("Would you like to continue forward? (Y/N)");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
